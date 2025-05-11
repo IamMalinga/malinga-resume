7898abd19c1ec7a9b8b123d90c19dcfcd2800557
Malinga <malinga_samarakoon@outlook.com>
2025-05-12 00:47:34 +0530

    Update github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  test: {
+    globals: true,
+    environment: 'jsdom', // Simulates a browser environment for React
+    setupFiles: './src/setupTests.js', // Optional: for custom setup
+  },
 })
